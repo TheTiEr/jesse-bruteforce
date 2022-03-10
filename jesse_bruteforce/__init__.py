a16108597dbb5366b13191fcc2e55b5a5f63635f
Tier <mail@timo-erdmann.de>
2022-03-10 18:37:54 +0100

    update

--- jesse_bruteforce/__init__.py
+++ jesse_bruteforce/__init__.py
@@ -93,7 +93,7 @@ def run() -> None:
 
     hyper = hyperactive.Hyperactive(distribution="joblib")
 
-    if cfg['optimizer'] in ["EvolutionStrategyOptimizer"]:
+    if cfg['optimizer'] in ["EvolutionStrategyOptimizer", "RandomRestartHillClimbingOptimizer"]:
         if cfg['optimizer'] == "EvolutionStrategyOptimizer":
             optimizer = hyperactive.optimizers.EvolutionStrategyOptimizer(
                 population=cfg[cfg['optimizer']]['population'],
@@ -102,9 +102,18 @@ def run() -> None:
                 rand_rest_p=cfg[cfg['optimizer']]['rand_rest_p'],
             )
 
-        if mem is None or len(mem) < cfg[cfg['optimizer']]['population']:
-            if mem is not None and len(mem) < cfg[cfg['optimizer']]['population']:
-                print('Previous optimization has too few individuals for population. Reinitialization necessary.')
+        elif cfg['optimizer'] == "RandomRestartHillClimbingOptimizer":
+            optimizer = hyperactive.optimizers.RandomRestartHillClimbingOptimizer(
+                epsilon=0.1,
+                distribution="laplace",
+                n_neighbours=4,
+                rand_rest_p=0.1,
+                n_iter_restart=20,
+            )
+
+        if mem is None or len(mem) < cfg[cfg['optimizer']]['population'] or True:
+            #if mem is not None and len(mem) < cfg[cfg['optimizer']]['population']:
+            #    print('Previous optimization has too few individuals for population. Reinitialization necessary.')
             # init empty pandas dataframe
             search_data = pd.DataFrame(columns=[k for k in search_space.keys()] + ["score"] + [f'training_{k}' for k in empty_backtest_data.keys()] + [f'testing_{k}' for k in empty_backtest_data.keys()])
             with open(path, "w") as f:
